4fa3646790291dbf4583cbcafb8dd6d78e1f7ed8
yygao <>
2013-01-11 16:31:30 +0000

    adding 1+

--- TUtil.cc
+++ TUtil.cc
@@ -347,7 +347,7 @@ double JHUGenMatEl(TVar::Process process, mcfm_event_type* mcfm_event, double MR
   if ( process == TVar::TZZ_4l ) {
     __modgraviton_MOD_evalamp_gg_g_vv(p4, &MReso,  &GaReso, xggcoupl, xvvcoupl, MYIDUP, &MatElSq);
   }
-  if ( process == TVar::VZZ_4l ) {
+  if ( process == TVar::VZZ_4l || process == TVar::AVZZ_4l) {
     // -- YY: note that even if it is called xggcouplings, we are only testing xqq!
     __modzprime_MOD_evalamp_qqb_zprime_vv(p4, &MReso,  &GaReso, xggcoupl, xvvcoupl, MYIDUP, &MatElSq);
   }
