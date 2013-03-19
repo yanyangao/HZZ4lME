6079ab43357a372c25302447a25b2c7a6ec5e0a5
yygao <>
2013-03-19 16:36:07 +0000

    clean slightly

--- MELArocCurve.C
+++ MELArocCurve.C
@@ -4,28 +4,19 @@
 // YY will clean it if she is in a good cleaning mood
 // 
 
-//char* fileName = "1plus";
 //char* fileName = "1minus";
-char* fileName = "minGrav";
-//char* fileName = "minGravqq";
+//char* fileName = "1plus";
+//char* fileName = "minGrav";
+char* fileName = "minGravqq";
 
 TGraph* makeROCcurve(char* drawVar="melaLD", 
 		     const int bins=100, float start=0, float end=1,
 		     int lineColor=1, int lineStyle=1, int lineWidth=2,	int flavor = 4, 
 		     char* sigFile="SMHiggs_comb_withDiscriminants_ME.root",
-		     char* bkgFile="minGrav_comb_withDiscriminants_ME.root",
-		     // char* bkgFile="minGravqq_comb_withDiscriminants_ME.root",
 		     // char* bkgFile="minGrav_comb_withDiscriminants_ME.root",
-		     // char* sigFile="fromAndrew/SMHiggs_comb_withDiscriminants_withDiscriminants_ME.root",
-		     // char* bkgFile="fromAndrew/minGravqq_comb_withDiscriminants_withDiscriminants_ME.root",
-		     //char* sigFile="wresolution/SMHiggs_126GeV_comb_w4muResolution_withDiscriminants_ME.root",
-		     //char* bkgFile="wresolution/qqMinGrav_126GeV_comb_w4muResolution_withDiscriminants_ME.root",
-		     //char* bkgFile="fromAndrew/minGrav_comb_withDiscriminants_withDiscriminants_ME.root",
+		     char* bkgFile="minGravqq_comb_withDiscriminants_ME.root",
 		     // char* bkgFile="1minus_8-126_ME.root",
 		     //char* bkgFile="1plus_8-126_ME.root",
-		     // char* sigFile="fromYY/SMHiggs_8-126_withDiscriminants_ME.root",
-		     // char* bkgFile="fromYY/minGrav_8-126_withDiscriminants_ME.root",
-		     // char* bkgFile="fromYY/minGravqq_8-126_withDiscriminants_ME.root",
 		     ){
 
   TChain* SMHtree = new TChain("newTree");
@@ -96,58 +87,44 @@ void MELArocCurve(){
     // for spin 2 
     // 
 
+    /*
     // gg->X
-    // TGraph* MELA_check = makeROCcurve("spinTwoMinimalMelaLD",500,0,1,4,1,3,flavor);
     TGraph* MELA_check = makeROCcurve("Psmh/(2e-7*Ptwomplus_gg+Psmh)",500,0,1,4,1,3,flavor);
     TGraph* JHUgen = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+0.6*dXsec_TZZ_JHU)",500,0,1,1,2,3,flavor);
-    // TGraph* MELA_check_decay = makeROCcurve("spinTwoDecayMinimalMelaLD",500,0,1,2,1,3,flavor);
     TGraph* MELA_check_decay = makeROCcurve("Psmh/(Psmh+Ptwomplus_decay*1e-8)",500,0,1,2,1,3,flavor);
-    //TGraph* MELA_check_decay = makeROCcurve("spinTwoDecay",500,0,1,2,1,3,flavor);
     TGraph* JHUgen_decay = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+1e11*dXsec_TZZ_DECAY_JHU)",500,0,1,6,2,3,flavor);
-
-
-    /*
-    // qqbar->X 
-    // TGraph* MELA_check = makeROCcurve("spinTwoqqMinimalMelaLD",500,0,1,4,1,3,flavor);
-    TGraph* MELA_check = makeROCcurve("Psmh/(2e-7*Ptwomplus_qq+Psmh)",500,0,1,4,1,3,flavor);
-    // TGraph* MELA_check = makeROCcurve("spinTwoQQ",500,0,1,4,1,3,flavor);
-    TGraph* JHUgen = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+20*dXsec_QQB_TZZ_JHU)",500,0,1,1,2,3,flavor);
-    // TGraph* MELA_check_decay = makeROCcurve("spinTwoDecayMinimalMelaLD",1000,0,0.15,2,1,3,flavor);
-    // TGraph* MELA_check_decay = makeROCcurve("spinTwoDecay",500,0,0.2,2,1,3,flavor);
-    TGraph* MELA_check_decay = makeROCcurve("Psmh/(Psmh+Ptwomplus_decay*1e-8)",500,0,1,2,1,3,flavor);
-    TGraph* JHUgen_decay = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+1e10*dXsec_TZZ_DECAY_JHU)",1000,0,0.6,6,2,3,flavor);
     */
 
-    MELA_check->Draw("AC");
-    JHUgen->Draw("sameC");
-    MELA_check_decay->Draw("sameC");
-    JHUgen_decay->Draw("sameC");
+    // qqbar->X 
+    TGraph* MELA_check = makeROCcurve("Psmh/(2e-7*Ptwomplus_qq+Psmh)",500,0,1,4,1,3,flavor);
+    TGraph* JHUgen = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+20*dXsec_QQB_TZZ_JHU)",500,0,1,1,2,3,flavor);
+    TGraph* MELA_check_decay = makeROCcurve("Psmh/(Psmh+Ptwomplus_decay*1e-8)",500,0,1,2,1,3,flavor);
+    TGraph* JHUgen_decay = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+1e10*dXsec_TZZ_DECAY_JHU)",500,0,1,6,2,3,flavor);
 
-    
-    /*
     //
     // for spin 1
     //
-
+    
+    /*
     // 1-
-    TGraph* MELA_check = makeROCcurve("Psmh/(Psmh+0.005*Poneminus)",100,0,1,4,1,3,flavor);
-    TGraph* MELA_check_decay = makeROCcurve("Psmh/(Psmh+0.0005*Poneminus_decay)",100,0,1,1,1,3,flavor);
-    TGraph* JHUgen = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+10*dXsec_VZZ_JHU)",100,0,1,2,2,3,flavor);
-    TGraph* JHUgen_decay = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+1e10*dXsec_VZZ_DECAY_JHU)",100,0,1,6,2,3,flavor);
-
+    TGraph* MELA_check = makeROCcurve("Psmh/(Psmh+0.005*Poneminus)",500,0,1,4,1,3,flavor);
+    TGraph* JHUgen = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+10*dXsec_VZZ_JHU)",500,0,1,1,2,3,flavor);
+    TGraph* MELA_check_decay = makeROCcurve("Psmh/(Psmh+0.0005*Poneminus_decay)",500,0,1,2,1,3,flavor);
+    TGraph* JHUgen_decay = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+1e10*dXsec_VZZ_DECAY_JHU)",500,0,1,6,2,3,flavor);
+    
     // 1+
-    TGraph* MELA_check = makeROCcurve("Psmh/(Psmh+0.005*Poneplus)",100,0,1,4,1,3,flavor);
-    TGraph* MELA_check_decay = makeROCcurve("Psmh/(Psmh+0.0005*Poneplus_decay)",100,0,1,1,1,3,flavor);
-    TGraph* JHUgen = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+10*dXsec_AVZZ_JHU)",100,0,1,2,2,3,flavor);
-    TGraph* JHUgen_decay = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+1e10*dXsec_AVZZ_DECAY_JHU)",100,0,1,6,2,3,flavor);
+    TGraph* MELA_check = makeROCcurve("Psmh/(Psmh+0.005*Poneplus)",500,0,1,4,1,3,flavor);
+    TGraph* JHUgen = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+10*dXsec_AVZZ_JHU)",500,0,1,1,2,3,flavor);
+    TGraph* MELA_check_decay = makeROCcurve("Psmh/(Psmh+0.0005*Poneplus_decay)",500,0,1,2,1,3,flavor);
+    TGraph* JHUgen_decay = makeROCcurve("dXsec_HZZ_JHU/(dXsec_HZZ_JHU+1e10*dXsec_AVZZ_DECAY_JHU)",500,0,1,6,2,3,flavor);
+
+    */        
 
     MELA_check->Draw("AC");
     MELA_check_decay->Draw("sameC");
     JHUgen->Draw("sameC");
     JHUgen_decay->Draw("sameC");
-    */    
     
-
     TLegend* leg = new TLegend(.2,.6,.5,.9);
     leg->SetFillColor(0);
     leg->AddEntry(MELA_check,"analytical(prod+decay)","l");
